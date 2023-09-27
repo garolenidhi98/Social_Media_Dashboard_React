226da5d7911926907b96ad43436bfb246a91d687
Nidhi Garole <nidhigarole004@gmail.com>
2023-09-27 18:18:02 +0530

    Unused imports removed

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import "./RightSide.css";
 import Home from "../../img/home.png";
-import Noti from "../../img/noti.png";
-import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
 import ShareModal from "../ShareModal/ShareModal";
